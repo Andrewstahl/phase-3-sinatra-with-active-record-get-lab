16c0d684c91457b34987b105f11c8dedf0d95035
Andrew Stahl <andrewstahl96@gmail.com>
2022-09-29 08:02:00 -0400

    All tests passing, files working with GET commands

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,31 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+  
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods' do
+    baked_goods = BakedGood.all
+    baked_goods.to_json
+  end
+  
+  get '/baked_goods/by_price' do
+    baked_good = BakedGood.all.order('price DESC')
+    baked_good.to_json(only: [:name, :price])
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_good = BakedGood.all.order('price DESC').limit(1)
+    baked_good.to_json
+  end
+
 
   # add routes
 
